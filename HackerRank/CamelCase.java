a914519ad28d31081942a1f9683d7d1c7364436a
Violeta TM <80209320+Violeta-Tejera@users.noreply.github.com>
2023-08-25 01:40:21 +0200

    Create CamelCase.java

---
+++ HackerRank/CamelCase.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        int counter = 1; // There must be at least one word (starting in lowercase)
+        int strlen = s.length();
+        
+        for(int i = 0; i < strlen; i++){
+            if(isUpperCase(s.charAt(i)) == true){
+                counter++;
+            }
+        }
+        
+        return counter;        
+    }
+    
+    static boolean isUpperCase(Character c){
+        if(c >= 'A' && c <= 'Z') return true;
+        
+        return false;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
